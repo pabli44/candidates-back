06e6f314ac6c7ee4f803779de2596649f96d1702
PabloVelilla <pablivg@gmail.com>
2026-01-14 22:50:59 -0500

    Starting CRUD operations and project components

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CandidatesController } from './candidates/candidates.controller';
+import { CandidatesService } from './candidates/candidates.service';
+import { CreateCandidateDto } from './candidates/dto/create-candidate.dto';
+import { CandidatesModule } from './candidates/candidates.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CandidatesModule],
+  controllers: [AppController, CandidatesController],
+  providers: [AppService, CandidatesService, CreateCandidateDto],
 })
 export class AppModule {}
---
+++ src/candidates/candidates.service.ts
@@ -0,0 +1,71 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Candidate } from './models/Candidate';
+
+
+@Injectable()
+export class CandidatesService {
+
+    candidates: Candidate[] = [
+        {
+            id: 1, 
+            name: 'John Doe', 
+            surname: 'Doe', 
+            seniority: 'Senior', 
+            years: 5, 
+            availability: true
+        },
+        { 
+            id: 2, 
+            name: 'Jane Smith', 
+            surname: 'Smith', 
+            seniority: 'Junior', 
+            years: 1, 
+            availability: false 
+        },
+        {
+            id: 3,
+            name: 'Bob Johnson',
+            surname: 'Johnson',
+            seniority: 'Mid-Level',
+            years: 3,
+            availability: true
+        }
+    ]
+
+    findAll(): Candidate[] {
+        return this.candidates;
+    }
+
+    findOne(id: number): Candidate {
+        const candidate = this.candidates.find(candidate => candidate.id === id);
+        if (!candidate) {
+            throw new NotFoundException(`Candidate with id ${id} not found`);
+        }
+
+        return candidate;
+    }
+
+    /*
+    create(candidate) {
+        this.candidates.push(candidate);
+        return candidate;
+    }
+
+    update(id: number, updatedCandidate) {
+        const index = this.candidates.findIndex(candidate => candidate.id === id);
+        if (index !== -1) {
+            this.candidates[index] = updatedCandidate;
+            return updatedCandidate;
+        }
+        return null;
+    }*/
+
+    delete(id: number): Candidate {
+        const candidate = this.findOne(id);
+        this.candidates.splice(candidate.id - 1, 1);
+
+        return candidate;
+    }
+    
+
+}
