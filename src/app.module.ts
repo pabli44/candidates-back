d133174bb2e2491efef212b71ee81cfe431b1ccf
PabloVelilla <pablivg@gmail.com>
2026-01-16 20:14:17 -0500

    config for deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,16 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CandidatesController } from './candidates/candidates.controller';
+import { CandidatesService } from './candidates/candidates.service';
+import { CreateCandidateDto } from './candidates/dto/create-candidate.dto';
+import { UpdateCandidateDto} from './candidates/dto/update-candidate.dto';
 import { CandidatesModule } from './candidates/candidates.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, 
-    }),
-
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
-      }),
-    }),
-
-    CandidatesModule,
-  ],
+  imports: [CandidatesModule, MongooseModule.forRoot('mongodb+srv://pablivg-mongo:PabliMongodb@cluster0.qqe7v0u.mongodb.net/candidates_db?appName=Cluster0')],
+  controllers: [AppController, CandidatesController],
+  providers: [AppService, CandidatesService, CreateCandidateDto, UpdateCandidateDto]
 })
 export class AppModule {}
